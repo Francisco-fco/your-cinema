abe19a4dc132df81f2176faed2d8c574b142752b
FCO <franne_@hotmail.com>
2021-09-10 13:40:02 +0200

    fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="nav">
-    <router-link to="/">HOME</router-link> |
-    <router-link to="/about">COMING SOON</router-link>|
-    <router-link to="/about">SIGN UP</router-link>|
-    <router-link to="/about">GIFT CARDS</router-link>|
-    <router-link to="/about">ABOUT</router-link>
+    <img src="./assets/logo.png" />
+    <div id="routes">
+      <router-link to="/">HOME</router-link> |
+      <router-link to="/about">COMING SOON</router-link>|
+      <router-link to="/about">SIGN UP</router-link>|
+      <router-link to="/about">GIFT CARDS</router-link>|
+      <router-link to="/about">ABOUT</router-link>
+    </div>
 
     <!-- <div>
       <img src="./assets/breal.jpg" alt="">
     </div>
     -->
+    <div id="socialmedia">
+      <img src="./assets/Facebook60x60.png" />
+      <img src="./assets/Instagram60x60.png" />
+      <img src="./assets/Twitter60x60.png" />
+      <img src="./assets/YouTube60x60.png" />
+    </div>
   </div>
   <router-view />
 </template>
@@ -24,21 +33,37 @@
 }
 
 #nav {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
   padding: 5%;
   background-color: rgb(253, 76, 76);
-  
 }
 
-#nav a {
+#nav > #routes {
+  position: absolute;
+  left: 65vw;
+  top: 19vh;
+}
+
+#nav > #routes > a {
   font-weight: bold;
   color: #ffffff;
   text-decoration: none;
-  padding-right: 0.3vw;
+  width: 100%;
+  padding-right: 1vw;
+  padding-left: 1vw;
 }
 
 #nav a.router-link-exact-active {
   color: #050505;
   font-weight: bold;
+}
+
+#nav > #socialmedia {
+  position: absolute;
+  top: 8vh;
+  left: 75vw;
 }
 
 /*div > img {
